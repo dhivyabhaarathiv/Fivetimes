bc4a7b5589fcf8a0cb1150f14b215dc75c02c1f7
dhivyabhaarathiv <dhivyabhaarathiv@gmail.com>
2017-06-14 15:36:03 +0530

    Create Hellofive.java

---
+++ Hellofive.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hellofive
+{
+    public static void main(String args[])
+    {
+       // Scanner sc=new Scanner(System.in);
+        for(int i=0;i<5;i++)
+        {
+        System.out.println("Hello");
+        }
+        }
+        }
